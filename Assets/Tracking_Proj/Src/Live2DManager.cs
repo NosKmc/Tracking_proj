1787340e44c0d47a4906a03e82d3651279749110
nos <nos@kmc.gr.jp>
2019-11-07 00:13:15 +0900

    モデル動かしてみる

---
+++ Assets/Tracking_Proj/Src/Live2DManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Live2D.Cubism.Core;
+using Live2D.Cubism.Framework;
+
+public class Live2DManager : MonoBehaviour
+{
+    private CubismModel _model;
+    private float _t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _model = this.FindCubismModel();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        _t += (Time.deltaTime * 4f);
+        var value = Mathf.Sin(_t) * 30f;
+
+        var parameter = _model.Parameters[2];
+
+        parameter.Value = value;
+    }
+}
