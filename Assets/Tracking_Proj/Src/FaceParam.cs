01d4b30b08d2d212d83dd58b601888a43971a7a9
nos <nos@kmc.gr.jp>
2019-11-06 20:24:00 +0900

    MouthRatioを求める,RightEyeインデックスずれ

--- Assets/Tracking_Proj/Src/FaceParam.cs
+++ Assets/Tracking_Proj/Src/FaceParam.cs
@@ -7,6 +7,8 @@ using System;
 public class FaceParam
 {
     public float RightEyeRatio { get; private set; }
+    public float MouthRatio { get; private set; }
+
     public FaceParam()
     {
 
@@ -15,7 +17,24 @@ public class FaceParam
     {
         Point[] points = _points;
         Point[] eyePoints = new Point[6];
-        Array.Copy(points, 37, eyePoints, 0, 6);
-        this.RightEyeRatio = Point.DistancePow2(eyePoints[1], eyePoints[5]) / Point.DistancePow2(eyePoints[0], eyePoints[3]);
+        //右目は37番から
+        Array.Copy(points, 36, eyePoints, 0, 6);
+        this.RightEyeRatio = Point.DistancePow2(eyePoints[1], eyePoints[5]) 
+                                    / Point.DistancePow2(eyePoints[0], eyePoints[3]);
+    }
+
+    public void CalcMouthRatio(Point[] _points)
+    {
+        Point[] points = _points;
+        Point[] mouthPoints = new Point[6];
+        //口の内側は61番から
+        Array.Copy(points, 60, mouthPoints, 0, 8);
+        this.MouthRatio = Point.DistancePow2(mouthPoints[2], mouthPoints[6]) 
+                                    / Point.DistancePow2(mouthPoints[0], mouthPoints[4]);
+    }
+
+    public void CalcParams()
+    {
+
     }
 }
