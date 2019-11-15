b4940eaeb3f04b56ff665123ffdbba1099954705
nos <nos@kmc.gr.jp>
2019-11-15 23:21:47 +0900

    移動平均を実装

---
+++ Assets/Tracking_Proj/Src/MovingWindow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingWindow
+{
+    public int Size { get; private set; }
+    public float Value { get; private set; }
+    private Queue<float> window;
+
+    MovingWindow(int windowSize)
+    {
+        window = new Queue<float>();
+        Size = windowSize;
+    }
+
+    public float CalcValue(float val)
+    {
+        if (window.Count < Size)
+        {
+            window.Enqueue(val);
+            Value += val / Size;
+            return 0.0f;
+        }
+        window.Enqueue(val);
+        Value += (val - window.Dequeue()) / Size;
+        return Value;
+    }
+}
