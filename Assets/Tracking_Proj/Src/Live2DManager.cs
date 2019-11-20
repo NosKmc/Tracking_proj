2e0db76fd8b145787125cb9c683ded0965a65b1a
nos <nos@kmc.gr.jp>
2019-11-21 06:56:31 +0900

    モデル差し替え、パラメータ調整

--- Assets/Tracking_Proj/Src/Live2DManager.cs
+++ Assets/Tracking_Proj/Src/Live2DManager.cs
@@ -28,8 +28,15 @@ public class Live2DManager : MonoBehaviour
         _t += (Time.deltaTime * 4f);
         var value = Mathf.Sin(_t) * 30f;
         var parameter = _model.Parameters[2];
-        parameter.Value = value;*/
+        parameter.Value = value;
         var mouthParameter = _model.Parameters[18];
+        var reyeParameter = _model.Parameters[5];
+        var leyeParameter = _model.Parameters[3];
+        var faceYawParameter = _model.Parameters[0];
+        var facePitchParameter = _model.Parameters[1];
+        var faceRollParameter = _model.Parameters[2];
+        */
+        var mouthParameter = _model.Parameters[12];
         var reyeParameter = _model.Parameters[5];
         var leyeParameter = _model.Parameters[3];
         var faceYawParameter = _model.Parameters[0];
@@ -40,7 +47,7 @@ public class Live2DManager : MonoBehaviour
         reyeParameter.Value = faceParam.RightEyeRatio * 6 - 0.02f;
         leyeParameter.Value = faceParam.LeftEyeRatio * 6 - 0.02f;
         faceYawParameter.Value = faceParam.FaceYaw * -4.0f;
-        facePitchParameter.Value = (faceParam.FacePitch - 180) * 15.0f;
-        faceRollParameter.Value = faceParam.FaceRoll * 4.0f;
+        facePitchParameter.Value = (faceParam.FacePitch - 180.0f) * 13.0f;
+        faceRollParameter.Value = faceParam.FaceRoll * 3.0f;
     }
 }
